6ec01945e16ec421a29854467c5bde096863544d
Akshay Bhatia <110640709+akshaypkbhat@users.noreply.github.com>
2023-10-12 19:20:58 -0700

    Update martingale.py

--- martingale.py
+++ martingale.py
@@ -1,4 +1,7 @@
-import random
+import random, sys
+import matplotlib.pyplot as plt
+import numpy as np
+
 
 class MartingaleStrategy():
     
@@ -13,6 +16,8 @@ class MartingaleStrategy():
         self.LAST_BET = True
 
     def bet(self):
+
+        if self.cash <= 0: return
 
         self.bets += 1
         r = random.randint(0, 1)
@@ -31,19 +36,46 @@ class MartingaleStrategy():
 
         print(f' Bet: {self.bets} Cash: {self.cash} LAST_BET {self.LAST_BET} Bet Amount: {self.betAmount}')
 
-wins, losses = 0, 0
-for i in range(1000):
-    m = MartingaleStrategy(10_000, 1_000)
-    for i in range(10):
-        m.bet()
-    if m.cash > m.principal:
-        print(f'Gain of {m.cash - m.principal}')
-        print(f'Last Bet: {m.betAmount}')
-        wins += 1
-    elif m.cash < m.principal:
-        print(f'Loss of {m.cash - m.principal}')
-        losses += 1
-    else:
-        print('Broke Even')
+def main():
 
-print(f'wins: {wins} losses: {losses}')
+    ITERATIONS = int(sys.argv[1])
+    BETS = int(sys.argv[2])
+    PRINCIPAL = int(sys.argv[3])
+    STARTINGBET = int(sys.argv[4])
+
+    wins, losses = 0, 0
+    for i in range(ITERATIONS):
+        m = MartingaleStrategy(PRINCIPAL, STARTINGBET)
+        cash = []
+        bets = []
+        for i in range(BETS):
+            m.bet()
+            cash.append(m.cash)
+            bets.append(i)
+        if m.cash > m.principal:
+            print(f'Gain of {m.cash - m.principal}')
+            print(f'Last Bet: {m.betAmount}')
+            wins += 1
+        elif m.cash < m.principal:
+            print(f'Loss of {m.cash - m.principal}')
+            losses += 1
+        else:
+            print('Broke Even')
+        
+        plt.plot(bets, cash)
+
+        
+
+    print(f'wins: {wins} losses: {losses}')
+
+    # Matplotlib #
+
+    plt.xlabel("Bets")
+    plt.ylabel("Cash")
+
+    plt.xticks(np.arange(0, BETS, step=1))
+    plt.yticks(np.arange(0, 2*PRINCIPAL, step=PRINCIPAL/10))
+
+    plt.show()
+
+main()
